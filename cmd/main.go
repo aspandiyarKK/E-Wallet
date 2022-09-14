46001ff525dbc88a4db00e3cb367fec5a2168c82
aspandiyarKK <aspanprogress@gmail.com>
2022-09-14 14:26:19 +0600

    Fixing bugs and adding update and delete tests

--- cmd/main.go
+++ cmd/main.go
@@ -1,31 +1,43 @@
 package main
 
 import (
-	"EWallet/internal/rest"
-	"EWallet/pkg/logger"
-	"EWallet/pkg/repository"
+	"context"
 	"errors"
+	"fmt"
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"EWallet/internal/rest"
+	"EWallet/pkg/logger"
+	"EWallet/pkg/repository"
 )
 
-var pgDSN = os.Getenv("PG_DSN")
-var addr = "localhost:8080"
+const port = 3000
+
+var (
+	pgDSN = os.Getenv("PG_DSN")
+	addr  = fmt.Sprintf("localhost:%d", port)
+)
 
 func main() {
 	log := logger.NewLogger()
-	pg, err := repository.NewRepo(log, pgDSN)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	pg, err := repository.NewRepo(ctx, log, pgDSN)
 	if err != nil {
 		log.Panicf("Failed to connect to database: %v", err)
 	}
 	r := rest.NewRouter(log, pg)
-	if err := r.Run(addr); err != nil && !errors.Is(err, http.ErrServerClosed) {
+	if err = r.Run(ctx, addr); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Panicf("Error starting server: %v", err)
 	}
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGHUP, syscall.SIGQUIT)
 	<-sigCh
+	cancel()
+	pg.Close()
 	log.Info("Shutting down")
 }
---
+++ tests/integration_test.go
@@ -0,0 +1,158 @@
+package tests
+
+import (
+	"EWallet/internal/rest"
+	"EWallet/pkg/repository"
+	"bytes"
+	"context"
+	"encoding/json"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"net/http"
+	"strconv"
+	"testing"
+	"time"
+)
+
+const pgDSN = "postgres://postgres:secret@localhost:5433/postgres"
+
+type IntegrationTestSuite struct {
+	suite.Suite
+	log    *logrus.Logger
+	store  *repository.PG
+	router *rest.Router
+	url    string
+}
+
+func (s *IntegrationTestSuite) SetupSuite() {
+	ctx := context.Background()
+	s.log = logrus.New()
+	var err error
+	s.store, err = repository.NewRepo(ctx, s.log, pgDSN)
+	require.NoError(s.T(), err)
+	s.router = rest.NewRouter(s.log, s.store)
+	go func() {
+		_ = s.router.Run(ctx, "localhost:3001")
+	}()
+	s.url = "http://localhost:3001/api/v1"
+	time.Sleep(100 * time.Millisecond)
+}
+func (s *IntegrationTestSuite) TearDownSuite() {
+}
+
+func (s *IntegrationTestSuite) SetupTest() {
+}
+
+func (s *IntegrationTestSuite) TearDownTest() {
+}
+func (s *IntegrationTestSuite) TestCreateAndGetWallet() {
+	wallet := repository.Wallet{
+		Owner:   "test1",
+		Balance: 1050,
+	}
+	requestBody, err := json.Marshal(wallet)
+	require.NoError(s.T(), err)
+	path := s.url + "/wallet"
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, path, bytes.NewBuffer(requestBody))
+	require.NoError(s.T(), err)
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), http.StatusOK, resp.StatusCode)
+
+	var idMap map[string]int
+	err = json.NewDecoder(resp.Body).Decode(&idMap)
+	require.NoError(s.T(), err)
+
+	id, ok := idMap["id"]
+	require.True(s.T(), ok)
+	req, err = http.NewRequestWithContext(context.Background(), http.MethodGet, path+"/"+strconv.Itoa(id), nil)
+	require.NoError(s.T(), err)
+	resp, err = http.DefaultClient.Do(req)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), http.StatusOK, resp.StatusCode)
+	var walletResp repository.Wallet
+	err = json.NewDecoder(resp.Body).Decode(&walletResp)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), wallet.Owner, walletResp.Owner)
+	require.Equal(s.T(), wallet.Balance, walletResp.Balance)
+}
+
+func (s *IntegrationTestSuite) TestUpdateWallet() {
+	wallet := repository.Wallet{
+		Owner:   "test1",
+		Balance: 100,
+	}
+	wallet2 := repository.Wallet{
+		Owner:   "test2",
+		Balance: 1000,
+	}
+	requestBody, err := json.Marshal(wallet)
+	require.NoError(s.T(), err)
+	path := s.url + "/wallet"
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, path, bytes.NewBuffer(requestBody))
+	require.NoError(s.T(), err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), http.StatusOK, resp.StatusCode)
+
+	var idMap map[string]int
+	err = json.NewDecoder(resp.Body).Decode(&idMap)
+	require.NoError(s.T(), err)
+	id, ok := idMap["id"]
+	require.True(s.T(), ok)
+
+	requestBody, err = json.Marshal(wallet2)
+	req, err = http.NewRequestWithContext(context.Background(), http.MethodPut, path+"/"+strconv.Itoa(id), bytes.NewBuffer(requestBody))
+
+	require.NoError(s.T(), err)
+	resp, err = http.DefaultClient.Do(req)
+
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), http.StatusOK, resp.StatusCode)
+
+	var walletResp repository.Wallet
+	err = json.NewDecoder(resp.Body).Decode(&walletResp)
+	require.NoError(s.T(), err)
+
+	require.Equal(s.T(), walletResp.Owner, wallet2.Owner)
+	require.Equal(s.T(), walletResp.Balance, wallet2.Balance)
+
+}
+
+func (s *IntegrationTestSuite) TestDeleteWallet() {
+	wallet := repository.Wallet{
+		Owner:   "test1",
+		Balance: 100,
+	}
+
+	requestBody, err := json.Marshal(wallet)
+	require.NoError(s.T(), err)
+	path := s.url + "/wallet"
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, path, bytes.NewBuffer(requestBody))
+	require.NoError(s.T(), err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), http.StatusOK, resp.StatusCode)
+
+	var idMap map[string]int
+	err = json.NewDecoder(resp.Body).Decode(&idMap)
+	require.NoError(s.T(), err)
+	id, ok := idMap["id"]
+	require.True(s.T(), ok)
+
+	req, err = http.NewRequestWithContext(context.Background(), http.MethodDelete, path+"/"+strconv.Itoa(id), nil)
+	require.NoError(s.T(), err)
+	resp, err = http.DefaultClient.Do(req)
+	require.Equal(s.T(), http.StatusOK, resp.StatusCode)
+	require.NoError(s.T(), err)
+
+}
+func TestIntegrationTestSuite(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
